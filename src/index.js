26172123dbf0d170afc35ee2c577d2fc51c07dab
shengxiang <hzsunshx@corp.netease.com>
2015-12-22 23:12:20 +0800

    little sample code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-module.exports = "Hello"
+var PyObject = function(obj){
+	this._obj = obj;
+	// get or set method
+}
+
+var Connection = function(url){
+	this._url = url;
+	this.import = function(pkg){
+		// need json-rpc call
+		return new PyObject({
+			name: pkg
+		})
+	}
+}
+
+module.exports = function(url){
+	return new Connection(url);
+}
